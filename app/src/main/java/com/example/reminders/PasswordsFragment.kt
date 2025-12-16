02b4f138e5e8f2f031174a32c03c271eed9ac517
Feihuan Peng <feihuan.peng@spreetail.com>
2025-12-15 22:15:59 -0600

    asd

--- app/src/main/java/com/example/reminders/PasswordsFragment.kt
+++ app/src/main/java/com/example/reminders/PasswordsFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
+import com.example.reminders.databinding.DialogEditReminderBinding
 import com.example.reminders.databinding.FragmentPasswordsBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -34,17 +36,25 @@ class PasswordsFragment: Fragment() {
     }
 
     private fun displayValues() {
-        binding.textViewWifiValue.text = preferences.getString(PREF_WIFI, null)
-        binding.textViewWifiValue.text = preferences.getString(PREF_TABLET_PIN, null)
-        binding.textViewWifiValue.text = preferences.getString(PREF_BIKE_LOCK, null)
+        binding.textViewWifiValue.text = preferences.getString(PREF_WIFI, "")
+        binding.textViewTabletPinValue.text = preferences.getString(PREF_TABLET_PIN, "")
+        binding.textViewBikeLockValue.text = preferences.getString(PREF_BIKE_LOCK, "")
     }
 
     private fun showEditDialog(preferenceKey: String) {
+        val dialogBinding = DialogEditReminderBinding.inflate(requireActivity().layoutInflater).apply {
+            editTextValue.setText(preferences.getString(preferenceKey, ""))
+            editTextValue.setSelection(editTextValue.text?.length ?: 0)
+        }
+
         MaterialAlertDialogBuilder(requireContext())
             .setTitle("Update value")
-            .setView(null)
+            .setView(dialogBinding.root)
             .setPositiveButton("Save") { _, _ ->
-
+                preferences.edit(commit = true) {
+                    putString(preferenceKey, dialogBinding.editTextValue.text?.toString())
+                }
+                displayValues()
             }
             .setNegativeButton("Cancel") { _, _ ->
 
