dda441048ccd7043a3a204e00d7ce75e0f73ec36
Feihuan Peng <feihuan.peng@spreetail.com>
2025-12-15 21:52:59 -0600

    das

--- app/src/main/java/com/example/reminders/PasswordsFragment.kt
+++ app/src/main/java/com/example/reminders/PasswordsFragment.kt
@@ -1,15 +1,20 @@
 package com.example.reminders
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.reminders.databinding.FragmentPasswordsBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class PasswordsFragment: Fragment() {
 
     lateinit var binding: FragmentPasswordsBinding
+    private val preferences by lazy {
+        requireActivity().getSharedPreferences("passwords", Context.MODE_PRIVATE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +27,35 @@ class PasswordsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        displayValues()
+        binding.cardViewWifi.setOnClickListener { showEditDialog(PREF_WIFI) }
+        binding.cardViewTabletPin.setOnClickListener { showEditDialog(PREF_TABLET_PIN) }
+        binding.cardViewBikeLock.setOnClickListener { showEditDialog(PREF_BIKE_LOCK) }
+    }
+
+    private fun displayValues() {
+        binding.textViewWifiValue.text = preferences.getString(PREF_WIFI, null)
+        binding.textViewWifiValue.text = preferences.getString(PREF_TABLET_PIN, null)
+        binding.textViewWifiValue.text = preferences.getString(PREF_BIKE_LOCK, null)
+    }
+
+    private fun showEditDialog(preferenceKey: String) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Update value")
+            .setView(null)
+            .setPositiveButton("Save") { _, _ ->
+
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+
+            }
+            .show()
+    }
+
+    companion object {
+        const val PREF_WIFI = "pref_wifi"
+        const val PREF_TABLET_PIN = "pref_tablet_pin"
+        const val PREF_BIKE_LOCK = "pref_bike_lock"
     }
 
 }
